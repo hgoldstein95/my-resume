f2547b5e6bf11a32c0ef5087c1c614a9c2201f64
Harrison Goldstein <hgoldstein95@gmail.com>
2015-08-17 14:51:50 -0400

    Updated and improved gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,31 @@ var gulp = 					require('gulp');
 var sass = 					require('gulp-sass');
 var jade = 					require('gulp-jade');
 var browserSync = 	require('browser-sync').create();
-var plumber = 			require('gulp-plumber');
 var autoprefixer = 	require('gulp-autoprefixer');
 
+var sassPath = 'sass';
+var scssPath = 'sass';
+var cssPath = 'css'
+var jadePath = 'jade';
+
 gulp.task('jade', function() {
-	gulp.src('jade/*.jade')
-		.pipe(plumber())
+	gulp.src(jadePath + '/*.jade')
 		.pipe(jade())
+		.on('error', console.error.bind(console))
 		.pipe(gulp.dest('.'))
 		.pipe(browserSync.stream());
 });
 
 gulp.task('sass', function() {
-	gulp.src(['sass/*.sass', 'sass/*.scss'])
-		.pipe(plumber())
+	gulp.src([sassPath + '/*.sass', scssPath + '/*.scss'])
 		.pipe(sass())
+		.on('error', console.error.bind(console))
 		.pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-		.pipe(gulp.dest('css'))
+		.on('error', console.error.bind(console))
+		.pipe(gulp.dest(cssPath))
 		.pipe(browserSync.stream());
 });
 
@@ -30,13 +35,14 @@ gulp.task('browser-sync', function() {
 	browserSync.init({
 		server: {
 			baseDir: '.'
-		}
+		},
+		notify: false
 	})
 });
 
 gulp.task('watch', function() {
-	gulp.watch(['sass/*.sass', 'sass/*.scss'], ['sass']);
-	gulp.watch('jade/*.jade', ['jade']);
+	gulp.watch([sassPath + '/*.sass', scssPath + '/*.scss'], ['sass']);
+	gulp.watch(jadePath + '/*.jade', ['jade']);
 });
 
 gulp.task('default', ['sass', 'jade', 'watch', 'browser-sync']);
