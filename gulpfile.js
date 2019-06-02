27a7643b672aff461c387727b676abcde07ad2c5
Harrison Goldstein <hgoldstein95@gmail.com>
2019-06-02 14:32:05 -0400

    Fix some vulnerabilities, small updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,54 @@
-var gulp = 					require('gulp');
+var gulp = require("gulp");
 
-var sass = 					require('gulp-sass');
-var jade = 					require('gulp-jade');
-var browserSync = 	require('browser-sync').create();
-var autoprefixer = 	require('gulp-autoprefixer');
+var sass = require("gulp-sass");
+var jade = require("gulp-jade");
+var browserSync = require("browser-sync").create();
+var autoprefixer = require("gulp-autoprefixer");
 
-var sassPath = 'sass';
-var scssPath = 'sass';
-var cssPath = 'css'
-var jadePath = 'jade';
+var sassPath = "sass";
+var scssPath = "sass";
+var cssPath = "css";
+var jadePath = "jade";
 
-gulp.task('jade', function() {
-	gulp.src(jadePath + '/*.jade')
-		.pipe(jade())
-		.on('error', console.error.bind(console))
-		.pipe(gulp.dest('.'))
-		.pipe(browserSync.stream());
+gulp.task("jade", function() {
+  return gulp
+    .src(jadePath + "/*.jade")
+    .pipe(jade())
+    .on("error", console.error.bind(console))
+    .pipe(gulp.dest("."))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('sass', function() {
-	gulp.src([sassPath + '/*.sass', scssPath + '/*.scss'])
-		.pipe(sass())
-		.on('error', console.error.bind(console))
-		.pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-		.on('error', console.error.bind(console))
-		.pipe(gulp.dest(cssPath))
-		.pipe(browserSync.stream());
+gulp.task("sass", function() {
+  return gulp
+    .src([sassPath + "/*.sass", scssPath + "/*.scss"])
+    .pipe(sass())
+    .on("error", console.error.bind(console))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .on("error", console.error.bind(console))
+    .pipe(gulp.dest(cssPath))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('browser-sync', function() {
-	browserSync.init({
-		server: {
-			baseDir: '.'
-		},
-		notify: false
-	})
+gulp.task("browser-sync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "."
+    }
+  });
 });
 
-gulp.task('watch', function() {
-	gulp.watch([sassPath + '/*.sass', scssPath + '/*.scss'], ['sass']);
-	gulp.watch(jadePath + '/*.jade', ['jade']);
+gulp.task("watch", function() {
+  gulp.watch([sassPath + "/*.sass", scssPath + "/*.scss"], gulp.series("sass"));
+  gulp.watch(jadePath + "/*.jade", gulp.series("jade"));
 });
 
-gulp.task('default', ['sass', 'jade', 'watch', 'browser-sync']);
+gulp.task(
+  "default",
+  gulp.parallel(gulp.series("sass", "jade", "watch"), "browser-sync")
+);
